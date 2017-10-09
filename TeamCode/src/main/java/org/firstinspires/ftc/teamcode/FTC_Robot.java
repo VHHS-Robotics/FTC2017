f707b2545a161c14b20f954b8debd489f07beeb4
kyle.ramstad@lausd.net <kyle.ramstad@lausd.net>
2017-10-09 16:09:01 -0700

    Add relic arm motor and servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_Robot.java
@@ -209,6 +209,7 @@ public class FTC_Robot extends LinearOpMode {
                 SmallRelicServo.setPosition(SmallRelicServo.getPosition()-0.05);
             }
 
+
             // Display the current value
             telemetry.addData("Motor Right", "%5.2f", right);
             telemetry.addData("Motor Left Power", "%5.2f", left);
